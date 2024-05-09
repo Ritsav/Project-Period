979f18d75873585cd9fb4b2dc1c6b78c3fee7111
Happy_soul <60186486+Ritsav@users.noreply.github.com>
2024-05-09 21:38:14 +0545

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include "functions.h"
+
 #include "stdio.h"
-#include "stdlib.h"
 #include "conio.h"
 #include "string.h"
 
 typedef struct{
+    int id;
     char username[50];
     char password[50];
     char email[50];
@@ -17,23 +17,23 @@ int main(){
     char ch;
 
     // FILE OPEN
-    FILE *fptr = fopen("Users.txt", "a+");
+    FILE *file1 = fopen("Users.txt", "a+");
 
     // FILE status check
-    if (fptr == NULL) {
+    if (file1 == NULL) {
         printf("Error opening file.\n");
         return 1;
     }
 
     // FILE '\n' length for LOOP
     do{
-        ch = fgetc(fptr);
+        ch = fgetc(file1);
         if(ch == '\n'){
             len++;
         }
     }while(ch != EOF);
 
-    rewind(fptr);
+    rewind(file1);
 
     // Choose Either Register OR Login
     printf("Type the action you want to perform: \n");
@@ -45,17 +45,19 @@ int main(){
     // Register & Login
     if(logAction == 1){
 
+        fprintf(file1, "Id: %d ", len + 1);
+
         printf("Username: ");
         scanf("%s", user.username);
-        fprintf(fptr, "Username: %s ", user.username);
+        fprintf(file1, "Username: %s ", user.username);
 
         printf("Email: ");
         scanf("%s", user.email);
-        fprintf(fptr, "Email: %s ", user.email);
+        fprintf(file1, "Email: %s ", user.email);
 
         printf("Password: ");
         scanf("%s", user.password);
-        fprintf(fptr, "Password: %s\n", user.password);
+        fprintf(file1, "Password: %s\n", user.password);
 
     }else if(logAction == 2){
 
@@ -67,7 +69,7 @@ int main(){
 
         int found = 0;
         for (int i = 0; i < len; i++) {
-            fscanf(fptr, "Username: %s Email: %s Password: %s\n", checkUser.username, checkUser.email, checkUser.password);
+            fscanf(file1, "Id: %d Username: %s Email: %s Password: %s\n", &checkUser.id, checkUser.username, checkUser.email, checkUser.password);
 
             if (strcmp(user.username, checkUser.username) == 0 && strcmp(user.password, checkUser.password) == 0) {
                 found = 1;
@@ -75,13 +77,24 @@ int main(){
             }
         }
 
-        if (found) {
-            printf("Login successful!\n");
+        if(found) {
+            // Dynamically create the filename
+            char filename[50];
+            snprintf(filename, sizeof(filename), "User%d.txt", checkUser.id);
+
+            FILE *file2 = fopen(filename, "a+");
+            if (file2 == NULL) {
+                printf("Error opening file.\n");
+                return 1;
+            }
+
+            fclose(file2);
         } else {
             printf("Login failed!\n");
         }
 
     }
 
+    fclose(file1);
     return 0;
 }
